217c985d15fbe8b9e56be6fbc8ce7ae6a8e2da05
Trmrskr <simonalale@gmail.com>
2023-06-06 09:45:19 -0700

    scores model

---
+++ models/scores.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This file holds the model for Scores"""
+
+
+
+class Scores(BaseModel):
+    """Class model of work"""
+
+    __tablename__ = "scores"
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    score_id = Column(String(60), ForeignKey('scores.id'), nullable=False)
+    exam_id = Column(String(60), ForeignKey('exams.id'), nullable=False)
+    subject_id = Column(String(60), ForeignKey('subjects.id'), nullable=False)
+    score = Column(Integer, nullable=False, default=0)
+    timestamp = Column(Datetime, nullable=False, default=(datetime.utcnow()))
