afd4ba2b77882fa60ba6888ac093705baf599742
Trmrskr <simonalale@gmail.com>
2023-06-07 11:03:26 -0700

    model for subject

--- models/subject.py
+++ models/subject.py
@@ -2,7 +2,8 @@
 """This file holds the class model Subject"""
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-import models
+from models.base_model import BaseModel
+from models.exam import Exam
 
 
 
@@ -10,6 +11,6 @@ class Subject(BaseModel):
     """The namespace of the class model"""
 
     __tablename__ = "subjects"
-    exam_id = Column(String(60), ForeignKey="exams.id", nullable=False)
+    exam_id = Column(String(60), ForeignKey("exams.id"), nullable=False)
     name = Column(String(64), nullable=False)
     faculty = Column(String(64), nullable=False)
