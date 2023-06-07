9b4ced78d2cdd9df69caa202f4581ae2ffac2023
Trmrskr <simonalale@gmail.com>
2023-06-07 11:01:25 -0700

    exam model editted

--- models/exam.py
+++ models/exam.py
@@ -5,17 +5,20 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import String, Column, Integer
 from sqlalchemy.orm import relationship
 from sqlalchemy import ForeignKey
-import models
+from models.base_model import BaseModel
+from models.user_exam import user_exam
+
 
 
 class Exam(BaseModel):
     """The class namespace of exam"""
     
-    __tablename__ = 'exam'
+
+    __tablename__ = 'exams'
     name = Column(String(512), nullable=False)
     duration = Column(Integer, nullable=False, default=60)
     institution_name = Column(String(64), nullable=False)
     instructions = Column(String(4096), nullable=False)
     scores = relationship('Score', backref='exam', cascade='all, delete, delete-orphan')
     subject = relationship('Subject', backref='exam', cascade='all, delete, delete-orphan')
-    user_exam = relationship("User", secondary=user_exam)
+    user_exams = relationship("User", secondary=user_exam)
