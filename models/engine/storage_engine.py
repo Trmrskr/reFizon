9e3d06253669759424c00791aae77f55af6a36b6
Trmrskr <simonalale@gmail.com>
2023-06-08 05:51:57 -0700

    Storage engine update

--- models/engine/storage_engine.py
+++ models/engine/storage_engine.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" The module manages the storage engine of the application"""
 
 from os import getenv
 from sqlalchemy.orm import sessionmaker, scoped_session
@@ -6,6 +7,7 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.ext.declarative import declarative_base
 
 class DBStorage:
+    """ This manages the storage engine"""
     __engine = None
     __session = None
 
@@ -16,7 +18,60 @@ class DBStorage:
         host = getenv("REFIZON_MYSQL_HOST")
         env = getenv("REFIZON_ENV")
 
-        self.__engine = create_engine('mysql+mysqldb://{}'.format(user, passwd, host, db), pool_pre_ping=True)
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                        .format(user, passwd, host, db),
+                                        pool_pre_ping=True)
 
         if env == "test":
             Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """returns a dictionary
+        Return:
+            returns a dictionary of __object
+        """
+        dic = {}
+        if cls:
+            if type(cls) is str:
+                cls = eval(cls)
+            query = self.__session.query(cls)
+            for elem in query:
+                key = "{}.{}".format(type(elem).__name__, elem.id)
+                dic[key] = elem
+        else:
+            classList = [User, Exam, Score, Subject, Question, Choice]
+            for item in classList:
+                query = self.__session.query(item)
+                for elem in query:
+                    key = "{}.{}".format(type(elem).__name__, elem.id)
+                    dic[key] = elem
+        return (dic)
+
+    def new(self, obj):
+        """add a new element in the table"""
+
+        self.__session.add(obj)
+
+    def save(self):
+        """Save changes to database"""
+
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Delete an element in the table"""
+
+        if obj:
+            self.session.delete(obj)
+
+    def reload(self):
+        """Load tables to database"""
+
+        Base.metadata.create_all(self.__engine)
+        sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sec)
+        self.__session = Session()
+
+    def close(self):
+        """Closes the database session"""
+
+        self.__session.close()
