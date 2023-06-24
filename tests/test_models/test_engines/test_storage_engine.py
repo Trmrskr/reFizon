3c49d3d9b7035c6e2d5a40896ef9f27ecf833adb
Trmrskr <simonalale@gmail.com>
2023-06-23 20:43:02 -0700

    unittesting codes

---
+++ tests/test_models/test_engines/test_storage_engine.py
@@ -0,0 +1,16 @@
+import unittest
+from model.engine.storage_engine import DBStorage
+from models.user import User
+
+class testStorageEngine(unittest.TestCase):
+
+    def setUp(self):
+
+
+    def test_new_save(self):
+
+    def test_reload(self):
+
+    def test_delete(self):
+
+    def test_all(self):
